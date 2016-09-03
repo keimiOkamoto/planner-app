99cbeaf657d380de0e909f54d718ca01bca7c27d
keimi.okamoto <keimi.okamoto@thomsonreuters.com>
2016-09-04 00:31:24 +0100

    Adds Employee and EmployeeContractType Enum

---
+++ src/main/java/Employee.java
@@ -0,0 +1,57 @@
+import java.time.LocalDate;
+
+public abstract class Employee {
+    private String id;
+    private String name;
+    private LocalDate dob;
+    private String employmentStartDate;
+    private EmploymentContractType employmentContractType;
+
+    public Employee(String id, String name, LocalDate dob, String employmentStartDate, EmploymentContractType employmentContractType) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.employmentStartDate = employmentStartDate;
+        this.employmentContractType = employmentContractType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getEmploymentStartDate() {
+        return employmentStartDate;
+    }
+
+    public void setEmploymentStartDate(String employmentStartDate) {
+        this.employmentStartDate = employmentStartDate;
+    }
+
+    public EmploymentContractType getEmploymentContractType() {
+        return employmentContractType;
+    }
+
+    public void setEmploymentContractType(EmploymentContractType employmentContractType) {
+        this.employmentContractType = employmentContractType;
+    }
+}
---
+++ src/main/java/EmploymentContractType.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Keimi on 04/09/2016.
+ */
+public enum EmploymentContractType {
+    FULL_TIME,
+    PART_TIME,
+    TEMPORATY,
+    CONTRACTOR
+}
