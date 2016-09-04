22b01ef5339c055a24590e23abd1c8ec70ea31ec
keimi.okamoto <keimi.okamoto@thomsonreuters.com>
2016-09-05 00:00:37 +0100

    Creates Shift class

---
+++ src/main/java/Shift.java
@@ -0,0 +1,22 @@
+import java.time.Duration;
+import java.util.List;
+
+public class Shift {
+    private String shiftId;
+    private Duration localDate;
+    private List<Employee> employees;
+
+    public Shift(String shiftId, Duration localDate, List<Employee> employees) {
+        this.shiftId = shiftId;
+        this.localDate = localDate;
+        this.employees = employees;
+    }
+
+    public String getShiftId() {
+        return shiftId;
+    }
+
+    public void setShiftId(String shiftId) {
+        this.shiftId = shiftId;
+    }
+}
