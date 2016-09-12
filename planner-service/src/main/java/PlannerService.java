a66a1f8d7bc58ce3bc3098adb0bd387cea74f49e
keimi.okamoto <keimiokamoto@gmail.com>
2016-09-13 00:04:14 +0100

    Adds initial behaviour to PlannerService

--- planner-service/src/main/java/PlannerService.java
+++ planner-service/src/main/java/PlannerService.java
@@ -1,8 +1,25 @@
 import domain.Planner;
+import domain.Shift;
+
 
 public class PlannerService {
 
+    private PlannerRepository plannerRepository = new PlannerRepository();
+
     public Planner createPlanner() {
-        return new Planner(IdGenerator.getId());
+        Planner planner = new Planner(IdGenerator.getId());
+        plannerRepository.put(planner);
+        return planner;
+    }
+
+    public void delete(String id) {
+        plannerRepository.delete(id);
+    }
+
+    public Planner getPlannerBy(String id) {
+        return plannerRepository.get(id);
+    }
+
+    public void addShift(Shift shift) {
     }
 }
--- planner-service/src/test/java/PlannerServiceTest.java
+++ planner-service/src/test/java/PlannerServiceTest.java
@@ -1,6 +1,12 @@
 import domain.Planner;
+import domain.Shift;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.Duration;
+import java.util.Collections;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,6 +14,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class PlannerServiceTest {
 
     private PlannerService plannerService = new PlannerService();
+    private PlannerRepository plannerRepository = new PlannerRepository();
 
     @Test
     public void shouldBeAbleToCreateNewPlanner() {
@@ -17,16 +24,27 @@ public class PlannerServiceTest {
 
     @Test
     public void shouldBeAbleToDeletePlanner() {
+        Planner planner = plannerService.createPlanner();
+        String id = planner.getId();
+        plannerService.delete(id);
 
+        Assert.assertNull(plannerRepository.get(id));
     }
 
     @Test
     public void shouldBeAbleToGetPlannerById() {
+        Planner expectedPlanner = plannerService.createPlanner();
+        String id = expectedPlanner.getId();
 
+        Planner actualPlanner = plannerService.getPlannerBy(id);
+
+        assertThat(expectedPlanner.getId(), is(equalTo(actualPlanner.getId())));
     }
 
     @Test
     public void shouldBeAbleToAddShiftToPlanner() {
+        Shift shift = new Shift("someID", Duration.ZERO, Collections.singletonList(new Contractor("123")));
+        plannerService.addShift(shift);
 
     }
 
