2ac3c599d3dd5b51bfb7d0ce03e76e4d5a7f3e5d
keimi.okamoto <keimiokamoto@gmail.com>
2016-09-14 15:54:38 +0100

    Adds company name to constructor and removes setter.

--- planner-service/src/main/java/domain/Planner.java
+++ planner-service/src/main/java/domain/Planner.java
@@ -3,15 +3,18 @@ package domain;
 public class Planner {
     private String id;
 
-    public Planner(String id) {
+    private String companyName;
+
+    public Planner(String id, String companyName) {
         this.id = id;
+        this.companyName = companyName;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getCompanyName() {
+        return companyName;
     }
 }
